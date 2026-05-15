34a645d0c338336d0483105f1dee8b1c7fa25b64
djamelsenouci-dev <djamel.senouci@gmail.com>
2026-05-15 23:13:13 +0200

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -14,7 +14,7 @@ export function HeroSection() {
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
-          className="text-center -mt-6"
+          className="text-center -mt-10"
         >
           <motion.div
             initial={{ opacity: 0, scale: 0.9 }}
